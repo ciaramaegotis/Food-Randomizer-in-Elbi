254e2050d3a049add1ff1b4ee0380c9cf2cd4b70
Ciara Mae Gotis <crgotis@up.edu.ph>
2018-01-28 19:05:18 +0800

    initial background image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Elbi Food Randomizer</h1>
-        </header>
         <p className="App-intro">
           To get started, just choose among the categories and begin the hard journey of choosing where to eat. <code>src/App.js</code> and save to reload.
         </p>
