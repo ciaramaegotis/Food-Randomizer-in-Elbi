db76cbc6f59e83ca58aa78076b2eb0a8ec0d3b9d
Ciara Mae Gotis <crgotis@up.edu.ph>
2018-01-28 18:49:49 +0800

    initial contents

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Elbi Food Randomizer</h1>
+        </header>
+        <p className="App-intro">
+          To get started, just choose among the categories and begin the hard journey of choosing where to eat. <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
